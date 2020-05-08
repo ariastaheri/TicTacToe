eb73bd5ba07eb758e5c7f8a0f6711e6d703e499b
Aria Sheikhtaheri <aria.staheri@gmail.com>
2020-05-07 23:26:14 -0400

    board logic added

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player(){
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(index: number) {
+    if(!this.squares[index]){
+      this.squares.splice(index, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+
+  calculateWinner() {
+    const winningLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    for(let i = 0; i < winningLines.length; i++){
+      const[valA, valB, valC] = winningLines[i];
+      if(this.squares[valA] &&
+        this.squares[valA] === this.squares[valB] &&
+        this.squares[valA] === this.squares[valC]) {
+          return this.squares[valA];
+        }
+        return null;
+    }
+  }
+}
